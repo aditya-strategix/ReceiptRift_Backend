38a7c05857569fee77f1570977d9ec14d4aee420
aditya-strategix <adityakumar65821@gmail.com>
2025-10-25 20:19:14 +0530

    dist updated

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ app.add_middleware(
     allow_headers=["*"]
 )
 unsplash_access_key=os.getenv("ACCESS_KEY")
-app.mount("/", StaticFiles(directory="static/dist", html=True), name="static")
+app.mount("/static", StaticFiles(directory="static/dist", html=True), name="static")
 
 @app.get("/")
 def read_root():
